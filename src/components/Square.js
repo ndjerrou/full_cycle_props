eac8a45a95e5f145b1d3daf68a6ee5b3098c6e1a
ndjerrou <n.djerrou@gmail.com>
2024-02-05 11:20:35 +0100

    exo

--- src/components/Square.js
+++ src/components/Square.js
@@ -5,6 +5,8 @@ const Square = props => {
       style={{
         width: '60px',
         height: '60px',
+        // the value of bgColor
+        // should be coming from the parent
         backgroundColor: 'black',
       }}
     >
